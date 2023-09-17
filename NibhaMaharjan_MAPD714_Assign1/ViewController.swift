a232f05a1d0cc4bb8fbbf7fee4c28e29a384fa78
Nibha Maharjan <nibhamaharjan@yahoo.com>
2023-09-17 11:47:32 -0400

    Initial Commit

---
+++ NibhaMaharjan_MAPD714_Assign1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NibhaMaharjan_MAPD714_Assign1
+//
+//  Created by nibha maharjan on 2023-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NibhaMaharjan_MAPD714_Assign1UITests/NibhaMaharjan_MAPD714_Assign1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NibhaMaharjan_MAPD714_Assign1UITestsLaunchTests.swift
+//  NibhaMaharjan_MAPD714_Assign1UITests
+//
+//  Created by nibha maharjan on 2023-09-17.
+//
+
+import XCTest
+
+final class NibhaMaharjan_MAPD714_Assign1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
